d138b3392c0b0b6e6b081eda54c422acc8c89289
badapodo <ksg6736@gmail.com>
2025-05-08 10:21:35 +0900

    feat: tiny.c adder관련 코드

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,10 +1,37 @@
-/*
- * adder.c - a minimal CGI program that adds two numbers together
- */
-/* $begin adder */
 #include "csapp.h"
 
 int main(void) {
-  exit(0);
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    // QUERY_STRING 환경변수에서 인자 추출
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+        p = strchr(buf, '&');
+        *p = '\0';
+        strcpy(arg1, buf);
+        strcpy(arg2, p + 1);
+        n1 = atoi(strchr(arg1, '=') + 1);
+        n2 = atoi(strchr(arg2, '=') + 1);
+    }
+
+    // HTML 콘텐츠 생성
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content + strlen(content),
+            "THE Internet addition portal.\r\n<p>");
+    sprintf(content + strlen(content),
+            "The answer is: %d + %d = %d\r\n<p>", n1, n2, n1 + n2);
+    sprintf(content + strlen(content),
+            "Thanks for visiting!\r\n");
+
+    // HTTP 응답 헤더 출력
+    printf("Connection: close\r\n");
+    printf("Content-length: %lu\r\n", strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+
+    // HTML 본문 출력
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
 }
-/* $end adder */
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -1,43 +1,160 @@
-/* $begin tinymain */
-/*
- * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
- *     GET method to serve static and dynamic content.
- *
- * Updated 11/2019 droh
- *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
- */
 #include "csapp.h"
 
-void doit(int fd);
-void read_requesthdrs(rio_t *rp);
-int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
-void get_filetype(char *filename, char *filetype);
-void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
+void serve_ssr_adder(int fd, char *cgiargs) {
+    char buf[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    if (sscanf(cgiargs, "n1=%d&n2=%d", &n1, &n2) != 2) {
+        n1 = n2 = 0;
+    }
+
+    sprintf(content,
+        "<html><body>"
+        "<h1>SSR Adder Result</h1>"
+        "<p>The answer is: %d + %d = <strong>%d</strong></p>"
+        "<p><a href=\"/adder.html\">Go Back</a></p>"
+        "</body></html>", n1, n2, n1 + n2);
+
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %lu\r\n\r\n", strlen(content));
+    Rio_writen(fd, buf, strlen(buf));
+
+    Rio_writen(fd, content, strlen(content));
+}
+
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+    char *ptr;
+    if (strstr(uri, "/ssr_adder")) {
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr + 1);
+            *ptr = '\0';
+        } else {
+            strcpy(cgiargs, "");
+        }
+        strcpy(filename, "./ssr_adder"); // dummy filename
+        return 2; // SSR
+    }
+    if (!strstr(uri, "cgi-bin")) {
+        strcpy(cgiargs, "");
+        sprintf(filename, ".%s", uri);
+        if (uri[strlen(uri) - 1] == '/')
+            strcat(filename, "home.html");
+        return 1; // static
+    }
+    ptr = index(uri, '?');
+    if (ptr) {
+        strcpy(cgiargs, ptr + 1);
+        *ptr = '\0';
+    } else {
+        strcpy(cgiargs, "");
+    }
+    sprintf(filename, ".%s", uri);
+    return 0; // CGI
+}
+
+void serve_static(int fd, char *filename, int filesize, char *method) {
+    char filetype[MAXLINE], buf[MAXBUF];
+    int srcfd;
+
+    get_filetype(filename, filetype);
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    sprintf(buf + strlen(buf), "Server: Tiny\r\n");
+    sprintf(buf + strlen(buf), "Content-length: %d\r\n", filesize);
+    sprintf(buf + strlen(buf), "Content-type: %s\r\n\r\n", filetype);
+    Rio_writen(fd, buf, strlen(buf));
+
+    if (!strcasecmp(method, "HEAD")) return;
+
+    srcfd = Open(filename, O_RDONLY, 0);
+    char *srcbuf = malloc(filesize);
+    Rio_readn(srcfd, srcbuf, filesize);
+    Close(srcfd);
+    Rio_writen(fd, srcbuf, filesize);
+    free(srcbuf);
+}
+
+void get_filetype(char *filename, char *filetype) {
+    if (strstr(filename, ".html"))
+        strcpy(filetype, "text/html");
+    else if (strstr(filename, ".js"))
+        strcpy(filetype, "application/javascript");
+    else
+        strcpy(filetype, "text/plain");
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
+    char buf[MAXLINE], body[MAXBUF];
+
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body + strlen(body), "<body><p>%s: %s</p><p>%s: %s</p></body></html>\r\n",
+            errnum, shortmsg, longmsg, cause);
+
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %lu\r\n\r\n", strlen(body));
+    Rio_writen(fd, buf, strlen(buf));
+    Rio_writen(fd, body, strlen(body));
+}
+
+void doit(int fd) {
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    struct stat sbuf;
+    rio_t rio;
+
+    Rio_readinitb(&rio, fd);
+    Rio_readlineb(&rio, buf, MAXLINE);
+    sscanf(buf, "%s %s %s", method, uri, version);
+
+    if (strcasecmp(method, "GET")) {
+        clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+        return;
+    }
+
+    while (strcmp(buf, "\r\n"))
+        Rio_readlineb(&rio, buf, MAXLINE);
+
+    int type = parse_uri(uri, filename, cgiargs);
+
+    if (type == 2) {
+        serve_ssr_adder(fd, cgiargs);
+        return;
+    }
+
+    if (stat(filename, &sbuf) < 0) {
+        clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+        return;
+    }
+
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+        clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read this file");
+        return;
+    }
+
+    serve_static(fd, filename, sbuf.st_size, method);
+}
 
 int main(int argc, char **argv) {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
 
-  /* Check command line args */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
 
-  listenfd = Open_listenfd(argv[1]);
-  while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);  // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
-  }
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        doit(connfd);
+        Close(connfd);
+    }
 }
